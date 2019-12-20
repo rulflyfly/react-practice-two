b2e674e37ebdeac3664a87bbb9564a2fd26564da
Anastasia <user@MacBook-Pro-User.local>
2019-12-20 11:03:13 +0300

    Updates

---
+++ precache-manifest.1c78a2d5e4b0c870e907a5e6f56302fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e674d4522971a797dd442e6ec933df59",
+    "url": "/react-practice-two/index.html"
+  },
+  {
+    "revision": "ddbb9638ef0bf4ac2063",
+    "url": "/react-practice-two/static/css/main.d9745003.chunk.css"
+  },
+  {
+    "revision": "e50a7ab6a0d8016678b1",
+    "url": "/react-practice-two/static/js/2.6bbc632a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-practice-two/static/js/2.6bbc632a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddbb9638ef0bf4ac2063",
+    "url": "/react-practice-two/static/js/main.05173869.chunk.js"
+  },
+  {
+    "revision": "00ad01c08f0087d5fa6d",
+    "url": "/react-practice-two/static/js/runtime-main.389b7231.js"
+  }
+]);
